a2f96833abc1bac2e721d95d61ca421998c56594
DeerFreckles <deerfreckles@protonmail.com>
2021-06-06 17:59:12 -0400

    Pythonian Pair-Sums Problem Solution

--- pair_sums.py
+++ pair_sums.py
@@ -4,7 +4,6 @@ def numberOfWays(arr, k):
     out = 0
     for i in range(0, len(arr)-1):
         for j in range(i+1, len(arr)):
-            print(i,j)
             if arr[i]+arr[j] == k:
                 out += 1
     return out
